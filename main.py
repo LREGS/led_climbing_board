ebf20914da994ad312b03d32f9a3d0091e62089f
Lregs <william-cullum@hotmail.co.uk>
2023-05-11 22:02:58 +0100

    attempted to add climb creation functionality. When the create climb button is clicked it creates a state in which the programme should expect either 1) the user to select the holds for their climb or 2) the user to not want to select the holds and to continue normal function (at this point the hold buttons should be disabled. Once the holds have entered into an active state im trying to push the object into the hand_hold_selection function and use the sender method to get the hold name the user has clicked on and store it in a list as a value for the route key within the climb dictionary, however, its currently sending the button press as a boolean of true or false rather than whole object

--- main.py
+++ main.py
@@ -8,9 +8,45 @@ import os
 from create_clmb_dlg import CreateClimbDlg
 import time
 
+class State:
+    """Tracks the current state of the UI to determine behaviour"""
+    def __init__(self):
+        #Checks whether the user is trying to create a climb
+        self.creation_actv = False 
+
+class Climb():
+    def __init__(self, state):
+        self.climb = {'route' : []}
+        self.state = state
+                
+    def handle_hold_selection(self):
+        """Stores the desired holds for the climb into a dictionary"""
+        hold_name = self.sender().objectName()
+        self.climb['route'].append(hold_name)
+        print(hold_name)
+    
+    def assign_start_holds(self):
+        print('n/a')
+    
+    def assign_finish_holds(self):
+        print('n/a')
+    
+    def assign_grade(self):
+        print('n/a')
+    
+    def add_rating(self):
+        print('n/a')
+    
+    def write_to_csv(self, filename):
+        print('n/a')
+    
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        
+        #Sets the main windows state
+        self.state = State()
 
         # Load UI file
         ui_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "boardgui.ui"))
@@ -18,6 +54,9 @@ class MainWindow(QMainWindow):
         ui_file = QFile(ui_file_path)
         ui_file.open(QIODevice.ReadOnly)
         self.board_widget = loader.load(ui_file)
+        # Close the file
+        ui_file.close()    
+           
 
     
 
@@ -30,71 +69,49 @@ class MainWindow(QMainWindow):
             
         
         
-        #Add Buttons on Climbs
+        #Adds Buttons for Holds
         board_holds = []
         for i in range(1, 28):
             holds = "hold{}".format(i)
             hold = self.board_widget.findChild(QToolButton, holds)
             board_holds.append(hold)
             hold.raise_()
-            hold.clicked.connect(self.hold_selected)
-        
-        """Getting messy here - do  I need to add a function to create buttons\
-            into another module?!
-            
-            The buttons also look really ugly in the UI as they span the whole 
-            window. Need adding into a toolbar or making in QtDesigner 
-            so they don't look so dumb
-            
-            """
-        
-        
+            #hold.clicked.connect(self.hold_selected)
+            #Checks whether the user is trying to create a climb
+            climb = Climb(self.state)
+            hold.clicked.connect(Climb.handle_hold_selection)
+              
         
         #Adds a Create Climb Button 
         self.create_climb_btn = QPushButton('Create Climb')
         main_layout.addWidget(self.create_climb_btn)
         #Slot to link in to create_climbs
-        self.create_climb_btn.clicked.connect(self.create_climbs)
+        self.create_climb_btn.clicked.connect(self.create_climbs_state)
         
         #Adds a Save Climb button 
         self.save_climb_btn = QPushButton('Save Climb')
         main_layout.addWidget(self.save_climb_btn)
-       
-       
-       
-        # Close the file
-        ui_file.close()
+        
     
-    def hold_selected(self):
-        print('hold selected')
-    
-    #could/should maybe be a class def track climb, def choose start 
-    #def choose last hold def remove hold def illuminate button on click etc
-    def create_climbs(self):
-        """Starts tracking user input to create a climb ***POC*** """
+    def create_climbs_state(self):
+        """Initiates state in which hold buttons are active assuming user 
+            clicks ok"""
         dlg = CreateClimbDlg()
         
         #If user presses ok start tracking input
         if dlg.exec():
-            self.creation_actv = True
+            self.state.creation_actv = True
         else:
             #needs to link with the save climb button click to stop the tracking
-            self.creation_actv = False
+            self.state.creation_actv = False
         
         #Isolates the UI state to allow user to create climb    
         self.create_climb_btn.setEnabled(False)
-        
-        while self.creation_actv:
-            print('tracking clicks')
-            time.sleep(2)
-        #infinite loop - need to code the save climb button to stop the tracking
-        
-       
     
-           
+
         
 if __name__ == '__main__':
-    app = QApplication()
+    app = QApplication([])
     window = MainWindow()
     window.show()
     app.exec()
