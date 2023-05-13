8fe72973cae1e35471a2fe84d681e1447844c462
Lregs <william-cullum@hotmail.co.uk>
2023-05-13 20:43:50 +0100

    l

--- Climbs.py
+++ Climbs.py
@@ -4,15 +4,14 @@ QLabel,QPushButton, QToolButton, QVBoxLayout, QWidget, QDialog, QButtonGroup
 import os
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile, QIODevice
-from main import route
 
 
 class Climb():
     
     """Creates a Climb"""
     def __init__(self, button):
-        print(button)
-    
+        self.button = button
+
     def route(self, button):
         #self.hold_buttons.append(id(button))
         print(button)    
