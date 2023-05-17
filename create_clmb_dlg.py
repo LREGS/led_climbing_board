34987841878e4a2c82aec691f9727d6a3cebb3ef
Lregs <william-cullum@hotmail.co.uk>
2023-05-17 16:05:05 +0100

    have the button signal and slot now working within Main but want to tidy it up slightly so its modulated

--- create_clmb_dlg.py
+++ create_clmb_dlg.py
@@ -1,18 +1,18 @@
 from PySide6.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout, QLabel 
 from Climbs import Climb
-from main import MainWindow
 
 class CreateClimbDlg(QDialog):
     """Create a Dialog box that initilizes the create climb sequence"""
     def __init__(self):
         super().__init__()
-        
+        self. climb = None
         self.setWindowTitle('Create Climb')        
         #Create Buttons
         Btn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
         
         self.buttonBox = QDialogButtonBox(Btn)
-        self.buttonBox.accepted.connect(bAccepted)
+        
+        self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
         
         #Create Layout of Dialog box
@@ -22,15 +22,19 @@ class CreateClimbDlg(QDialog):
         self.layout.addWidget(self.buttonBox)
         self.setLayout(self.layout)
     
-def open_dlg_box():
-    dlg = CreateClimbDlg()
-    if dlg.exec() == QDialog.Accepted:
-        MainWindow.climbAccepted.emit()
-    else:
-        MainWindow.climbRejected.emit()
-
-def bAccepted():
-    print('a')
+    # def create_climb(self):
+    #     self.climb = Climb()
+    #     print('Please Select the Holds for your route and then click save')
+    #     return self.climb
+        
+    # def jon(self):
+    #     print('Rejected')
+    #     self.hide()
+          
+# def open_dlg_box():
+#     dlg = CreateClimbDlg()
+#     dlg.exec() 
+            
         
     
         
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PySide6 import QtCore
 from PySide6.QtWidgets import QSizePolicy, QApplication, QMainWindow, \
 QLabel,QPushButton, QToolButton, QVBoxLayout, QWidget, QDialog, QButtonGroup
 import os
-from create_clmb_dlg import CreateClimbDlg, open_dlg_box
+from create_clmb_dlg import CreateClimbDlg
 from Climbs import Climb
               
 
@@ -14,8 +14,35 @@ class MainWindow(QMainWindow):
 
              
     def __init__(self):
+        
+        def collect_route(button):
+           climb = Climb(route)
+           hold_selected = button_group.id(button)
+           route.append(hold_selected)
+           #climb.route.append(hold_selected)
+           #print(route)
+           climb.route = route
+        #prints route through instance of a climb
+           print(climb.route)
+        
+            #Adds Buttons for Holds
+        def create_btn_group(boardWidget):
+            hold_buttons = QButtonGroup()
+            for i in range(1, 28):
+                hold = boardWidget.findChild(QPushButton, f'hold{i}')
+                hold_buttons.addButton(hold, i)
+            return hold_buttons
+      
         super().__init__()
         
+        def open_create_clmb_dlg_box():
+            dlg = CreateClimbDlg()
+            result = dlg.exec()
+            
+            if result == QDialog.Accepted:
+                print('A')
+            else:
+                print('R')
 
         # Load UI file
         ui_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "boardgui.ui"))
@@ -33,50 +60,25 @@ class MainWindow(QMainWindow):
         self.central_widget.setLayout(main_layout)
         self.setCentralWidget(self.central_widget)
     
-        #Adds Buttons for Holds
-        hold_buttons = QButtonGroup()
-        for i in range(1, 28):
-            hold = self.board_widget.findChild(QPushButton, f'hold{i}')
-            hold_buttons.addButton(hold, i)
+        #Create Button Group for Holds
+        button_group = create_btn_group(board_widget)
         
-        
-        route = []
-        def collect_route(button):
-            climb = Climb(route)
-            hold_selected = hold_buttons.id(button)
-            route.append(hold_selected)
-            #climb.route.append(hold_selected)
-            #print(route)
-            climb.route = route
-        #prints route through instance of a climb
-            print(climb.route)
-        
-   
+        #Placeholder for the route list
+        route =[]
 
-        
-        
-        hold_buttons.buttonClicked.connect(collect_route)
+        #Connects the buttons to the collect_route slot
+        button_group.buttonClicked.connect(collect_route)
         
         #Adds a Create Climb Button 
         self.create_climb_btn = QPushButton('Create Climb')
         main_layout.addWidget(self.create_climb_btn)
         
         #Slot to link in to create_climbs
-        #self.create_climb_btn.clicked.connect(self.create_climbs_state)
-        self.create_climb_btn.clicked.connect(open_dlg_box)
-        self.climbAccepted.connect(self.handleAccepted)
-        
+        self.create_climb_btn.clicked.connect(open_create_clmb_dlg_box) 
         
         #Adds a Save Climb button 
         self.save_climb_btn = QPushButton('Save Climb')
         main_layout.addWidget(self.save_climb_btn)
-        
-        
-            
-    # def handleRejected(self):
-    #     print('r')   
-   
-
     
 
         
