1b1bddb5d2cf54e31de2fcdd94ce4035c81a462d
Lregs <william-cullum@hotmail.co.uk>
2023-05-25 20:20:02 +0100

    added climb and name as istances variables of SaveClimb that can be accessed in main py and then attributed to the Climb object

--- handle_save_climb.py
+++ handle_save_climb.py
@@ -8,12 +8,19 @@ from open_name_and_grade_dlg_box import get_name_grade
 class SaveClimb(QObject):
     """Handles Saving of the climb"""
     
+    def __init__(self):
+        self.climb_grade = None
+        self.climb_name = None
+        
+    
+    
     def handle_save_climb(self):
         
         grade, name = open_save_clmb_dlg_box()
         
         if grade and name:
-           print(f'{grade}, {name}')
+           self.climb_grade = grade
+           self.climb_name = name
         else:
             print('no name')
         
