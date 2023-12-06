80d1d8a8fa9e90d56ed871d508661be03320d955
William Cullum <william-cullum@hotmail.co.uk>
2023-12-06 20:22:00 +0000

    added ability to illuminate lights through flask server on rpi

--- tools/sendClimbRequest.py
+++ tools/sendClimbRequest.py
@@ -4,7 +4,7 @@ import json
 
 #have server working?!
 
-url = "http://localhost:5000/success"
+url = "http://192.168.1.130:8090/processRoute"
 route = {'start': '23', 'middle holds': (23,32,13,32), 'finish holds': (12,90)}
 
 
@@ -13,4 +13,4 @@ def sendRouteToServer(route, url):
     print(response.text)
 
 
-sendRouteToServer(route, url)
+# sendRouteToServer(route, url)
